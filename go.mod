322aba4bcf836c91ca7a9c513006964ab16c7528
Aaron Blondeau <aaronblondeau@gmail.com>
2023-09-03 10:45:07 -0600

    Add crew embed config.

--- go.mod
+++ go.mod
@@ -1,28 +1,41 @@
 module aaronblondeau.com/hasura-base-go
 
+// For local dev with crew-go in sibling folder:
+replace github.com/aaronblondeau/crew-go => ../../Dose/crew-go
+
 go 1.19
 
 require (
+	github.com/aaronblondeau/crew-go v1.0.4
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/iancoleman/strcase v0.0.0-20190422225806-e506e3ef7365
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.11.1
+	github.com/redis/go-redis/v9 v9.1.0
 	github.com/shopspring/decimal v1.3.1
 	github.com/steebchen/prisma-client-go v0.21.0
 	github.com/takuoki/gocase v1.0.0
+	golang.org/x/crypto v0.11.0
 	golang.org/x/text v0.11.0
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/go-co-op/gocron v1.27.1 // indirect
+	github.com/go-redsync/redsync/v4 v4.8.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/redis/go-redis/v9 v9.1.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +12,7 @@ import (
 	"syscall"
 
 	"aaronblondeau.com/hasura-base-go/controllers"
+	"github.com/aaronblondeau/crew-go/crew"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
@@ -75,6 +77,67 @@ func main() {
 		}
 	}
 
+	//////////////// Start Crew Setup
+
+	crewRedisAddress := os.Getenv("REDIS_ADDRESS")
+	if crewRedisAddress == "" {
+		crewRedisAddress = "localhost:6379"
+	}
+	crewRedisPassword := os.Getenv("REDIS_PASSWORD")
+
+	storage := crew.NewRedisTaskStorage(crewRedisAddress, crewRedisPassword, 1)
+	defer storage.Client.Close()
+
+	crewClient := crew.NewHttpPostClient()
+
+	throttlePush := make(chan crew.ThrottlePushQuery, 8)
+	throttlePop := make(chan crew.ThrottlePopQuery, 8)
+	crewThrottler := &crew.Throttler{
+		Push: throttlePush,
+		Pop:  throttlePop,
+	}
+
+	// No throttling for crew
+	go func() {
+		for {
+			select {
+			case pushQuery := <-throttlePush:
+				// Default behavior = immediate response => no throttling
+				fmt.Println("~~ Would throttle", pushQuery.Worker, pushQuery.TaskId)
+				pushQuery.Resp <- true
+			case popQuery := <-throttlePop:
+				fmt.Println("~~ Would unthrottle", popQuery.Worker, popQuery.TaskId)
+			}
+		}
+	}()
+
+	// Create the task controller (call to startup is further down)
+	crewController := crew.NewTaskController(storage, crewClient, crewThrottler)
+
+	// Validates each api call's Authorization header
+	crewAuthMiddleware := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			// For systems requiring no auth, just return next(c)
+			return next(c)
+		}
+	}
+
+	// Create the rest api server
+	inShutdown := false
+	watchers := make(map[string]crew.TaskGroupWatcher, 0)
+	crew.BuildRestApi(e, "/crew", crewController, crewAuthMiddleware, nil, &inShutdown, watchers)
+
+	// TODO - add worker routes to crewEcho
+
+	// Controller startup is performed after rest api is launched
+	// This is in case we switch TaskController.TriggerEvaluate to happen via an http call in scaled environments.
+	startupError := crewController.Startup()
+	if startupError != nil {
+		panic(startupError)
+	}
+
+	//////////////// End Crew Setup
+
 	// Hook into the shutdown signal
 	cleanupCompleteWg := sync.WaitGroup{}
 	done := make(chan os.Signal, 1)
@@ -88,6 +151,12 @@ func main() {
 		actionsController.Shutdown()
 		authController.Shutdown()
 
+		// Shutdown crew
+		for _, watcher := range watchers {
+			close(watcher.Channel)
+		}
+
+		// Shutdown echo
 		e.Close()
 
 		cleanupCompleteWg.Done()
@@ -96,7 +165,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "1323"
+		port = "3000"
 	}
 	e.Logger.Error(e.Start(host + ":" + port))
 
